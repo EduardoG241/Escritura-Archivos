a43162e12b79ff1590f4c90e24d46bcb05939a17
EduardoG241 <105743710+EduardoG241@users.noreply.github.com>
2022-06-02 11:26:40 -0700

    Escritura Archivos V1

---
+++ Escritura Archivos/Escritura Archivos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Escritura_Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt" , true);
+            // Si el archivo no existe, lo creará
+            // y  si ya es un archivo existente, escribira en el 
+
+            string[] lines =
+            {"Primera Linea"
+            , "Segunda Linea" 
+            , "Tercera Linea"};
+
+            foreach (string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo en el archivo...");
+            Console.ReadLine();
+        }
+    }
+}
